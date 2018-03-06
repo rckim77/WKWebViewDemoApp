388fece9c07c3558c2acc910d28e561d977a682c
rckim77 <rckim77@gmail.com>
2018-03-05 21:14:19 -0800

    adds localhost example for testing script message, adds comments

--- WKWebViewDemoApp/ViewController.swift
+++ WKWebViewDemoApp/ViewController.swift
@@ -16,13 +16,19 @@ class ViewController: UIViewController {
 
         let config = WKWebViewConfiguration()
         let userContentController = WKUserContentController()
+        
         config.userContentController = userContentController
 
+        // Load a local file containing JavaScript code as a string.
         guard let scriptPath = Bundle.main.path(forResource: "script", ofType: "js"),
             let scriptSource = try? String(contentsOfFile: scriptPath) else { return }
 
         let userScript = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
         userContentController.addUserScript(userScript)
+
+        // Add script message handlers that, when run, will make the function
+        // window.webkit.messageHandlers.<name>.postMessage() available in all frames.
+        userContentController.add(self, name: "test")
 
         let webView = WKWebView(frame: .zero, configuration: config)
 
@@ -36,15 +42,22 @@ class ViewController: UIViewController {
         webView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
         webView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
 
-        if let url = URL(string: "https://www.google.com") {
+        // Make sure in Info.plist you set `NSAllowsArbitraryLoads` to `YES` to load
+        // URLs with an HTTP connection.
+        if let url = URL(string: "http://localhost:8888/test.html") {
             webView.load(URLRequest(url: url))
         }
     }
 }
 
 extension ViewController: WKScriptMessageHandler {
+    // Capture postMessage() calls inside loaded JavaScript from the webpage. Note that a Boolean
+    // will be parsed as a 0 for false and 1 for true in the message's body. See WebKit documentation:
+    // https://developer.apple.com/documentation/webkit/wkscriptmessage/1417901-body.
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        print("received message: \(message.body)")
+        if let messageBody = message.body as? String {
+            print(messageBody)
+        }
     }
 
 
