1cb4213c6b383ba0b9b730c7b122a19eecd2ecd5
rckim77 <rckim77@gmail.com>
2018-03-05 20:06:23 -0800

    adds local js file, injects js, adds script message handler protocol

--- WKWebViewDemoApp/ViewController.swift
+++ WKWebViewDemoApp/ViewController.swift
@@ -14,7 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let webView = WKWebView(frame: .zero)
+        let config = WKWebViewConfiguration()
+        let userContentController = WKUserContentController()
+        config.userContentController = userContentController
+
+        guard let scriptPath = Bundle.main.path(forResource: "script", ofType: "js"),
+            let scriptSource = try? String(contentsOfFile: scriptPath) else { return }
+
+        let userScript = WKUserScript(source: scriptSource, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        userContentController.addUserScript(userScript)
+
+        let webView = WKWebView(frame: .zero, configuration: config)
 
         view.addSubview(webView)
 
@@ -26,9 +36,15 @@ class ViewController: UIViewController {
         webView.topAnchor.constraint(equalTo: layoutGuide.topAnchor).isActive = true
         webView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor).isActive = true
 
-        if let url = URL(string: "https://www.apple.com") {
+        if let url = URL(string: "https://www.google.com") {
             webView.load(URLRequest(url: url))
         }
+    }
+}
+
+extension ViewController: WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("received message: \(message.body)")
     }
 
 
